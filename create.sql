c67f516fd5a74efeb253458701faf7f86e7f94a6
HYPOLYMER <ghongmk900@gmail.com>
2013-11-03 17:42:33 +0900

    kmh

---
+++ create.sql
@@ -0,0 +1,86 @@
+/*	mysql -u root -p p2w
+	\. create.sql
+*/
+drop database p2w;
+create database p2w;
+use p2w
+
+CREATE TABLE basicoption1(
+	ID INT(20) NOT NULL AUTO_INCREMENT,
+	year INT(4),
+	serial VARCHAR(10),
+	PRIMARY KEY(ID,year,serial)
+);
+CREATE TABLE basicoption2(
+	ID INT(20) NOT NULL AUTO_INCREMENT,
+	type VARCHAR(10),
+	subject VARCHAR(20),
+	PRIMARY KEY(ID,type,subject)
+);
+CREATE TABLE classification(
+	ID INT(20) NOT NULL AUTO_INCREMENT,
+	subject VARCHAR(20),
+	large VARCHAR(50),
+	medium VARCHAR(50),
+	small VARCHAR(50),
+	PRIMARY KEY(ID,subject,large,medium,small)
+);
+CREATE TABLE problemheader(
+	problemID INT(20) NOT NULL AUTO_INCREMENT,
+	year INT(4),
+	serial VARCHAR(10),
+	type VARCHAR(10),
+	subject VARCHAR(20),
+	classify VARCHAR(20),
+	level VARCHAR(10),
+	large VARCHAR(50),
+	medium VARCHAR(50),
+	small VARCHAR(50),
+	pNum INT(5),
+	PRIMARY KEY(problemID,year,serial,type,subject,large,medium,small)
+);
+
+CREATE TABLE problembody(
+	problem VARCHAR(2000) NOT NULL,
+	addition VARCHAR (1000),
+	choice1 VARCHAR(500),
+	choice2 VARCHAR(500),
+	choice3 VARCHAR(500),
+	choice4 VARCHAR(500),
+	keyword VARCHAR(300),
+	solution VARCHAR(2000),
+	problemID INT(20) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(problemID)
+);
+
+
+/*
+<ATTRIBUTES>
+기출년도 year INT(4)
+회차 serial VARCHAR(10)
+유형 type VARCHAR(10)
+과목 subject VARCHAR(20)
+구분 classify VARCHAR(20)
+난이도 level VARCHAR(10)
+주요항목 large VARCHAR(50)
+세부항목 medium VARCHAR(50)
+세세항목 small VARCHAR(50)
+번호 pNum INT(5)
+문제-연결인덱스 problemID INT(20) NOT NULL AUTO_INCREMENT,
+문제 problem VARCHAR(2000) NOT NULL
+추가사항 addition VARCHAR (1000)
+보기	choice1 VARCHAR(500)
+	choice2 VARCHAR(500)
+	choice3 VARCHAR(500)
+	choice4 VARCHAR(500)
+풀이키워드 keyword VARCHAR(300)
+풀이 solution VARCHAR(2000)
+
+<RELATIONS>
+basicoption1(year,serial) +ID
+basicoption2(type,subject,classify,level) +ID
+classification(subject,large,medium,small) +ID
+problemheader(year,serial,type,subject,classify,level,large,medium,small,pNum) +problemID
+problembody(problem,addition,choice1,choice2,choice3,choice4,keyword,solution) +problemID
+
+*/
